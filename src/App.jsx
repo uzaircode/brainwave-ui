7422f0cabd77be330a238ab9f7cc344f92d2658c
Nik Uzair <nikuzairsc@gmail.com>
2024-04-07 23:30:10 +0800

    Header Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import ButtonGradient from './assets/svg/ButtonGradient';
+import Header from './components/Header';
+// import Hero from './components/Hero';
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <div className="pt-[4.75]rem lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        {/* <Hero /> */}
+      </div>
+
+      <ButtonGradient />
+    </>
+  );
 }
 
 export default App;
